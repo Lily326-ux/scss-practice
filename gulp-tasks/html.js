d12c871153e5e98869deb9b8d1cf08c34b807ff1
Lily326-ux <uprt.lily228@gmail.com>
2020-10-23 21:13:41 +0300

    add rigger, replace some makeup to shared folder

--- gulp-tasks/html.js
+++ gulp-tasks/html.js
@@ -7,10 +7,12 @@ const {
 // Libs
 const gulp = require('gulp');
 const { browserSync } = require('./server');
+const rigger = require('gulp-rigger');
 
 // Task
 const html = () => {
   return gulp.src(src.html)
+        .pipe(rigger())
         .pipe(gulp.dest(dist))
         .pipe(browserSync.reload({ stream:true }))
 }
